565956ad59194aedce6cff44e3a0d513367adfdc
Awkee <next4nextjob@gmail.com>
2023-05-01 22:19:09 +0800

    feat: 🎸 增加配置桌面主题功能
    增加配置桌面主题功能,支持 KDE主题配置

--- scripts/one4all.sh
+++ scripts/one4all.sh
@@ -21,6 +21,7 @@ os_codename=""      # Linux的Codename
 pac_cmd=""          # 包管理命令
 pac_cmd_ins=""      # 包管理命令
 cpu_arch=""         # CPU架构类型，仅支持x86_64
+gui_type=""         # GUI桌面环境类型
 
 # Define Colors
 RED='\e[41m'
@@ -123,8 +124,18 @@ function check_sys() { # 检查系统发行版信息，获取os_type/os_version/
                 ;;
         esac
     fi
+    case "$XDG_CURRENT_DESKTOP" in
+        KDE|GNOME|XFCE)
+            gui_type="$XDG_CURRENT_DESKTOP"
+            ;;
+        *)
+            gui_type=""
+            [[ "$$XDG_CURRENT_DESKTOP" == "" ]] && logerr "您当前会话类型为[$XDG_SESSION_TYPE] 非图形界面下运行"
+            [[ "$$XDG_CURRENT_DESKTOP" != "" ]] && logginfo "unknown desktop type: $XDG_CURRENT_DESKTOP"
+            ;;
+    esac
     cpu_arch="`uname -m`"
-    loginfo "os_type:$os_type,os_version:$os_version,cpu_arch=$cpu_arch, pac_cmd=$pac_cmd ."
+    loginfo "os_type:$os_type,os_version:$os_version,cpu_arch=$cpu_arch, DESKTOP=$gui_type, pac_cmd=$pac_cmd ."
     if [ -z "$pac_cmd" ] ; then
         return 1
     fi
@@ -348,6 +359,93 @@ function do_install_all() { # 安装菜单选择
             2) install_ohmyzsh      ;;
             3) install_tmux         ;;
             4) install_vim          ;;
+            q) return 0             ;;  # 返回上级菜单
+            *) redr_line "没这个选择[$str_answer],搞错了再来." ;;
+        esac
+    done
+}
+
+############# GUI环境配置部分 ####################################
+
+function config_kde_theme() {
+    loginfo "正在执行 config_kde_theme"
+    sudo $pac_cmd_ins latte-dock
+    tmp_path="/tmp/themes"    # 主题下载目录
+    mkdir -p $tmp_path
+    git clone https://github.com/numixproject/numix-kde-theme.git $tmp_path/numix-kde-theme && cd $tmp_path/numix-kde-theme && make install && cd -
+    
+    loginfo "安装全局主题-[WhiteSur-kde]"
+    git clone https://github.com/vinceliuice/WhiteSur-kde.git  $tmp_path/WhiteSur-kde && cd $tmp_path/WhiteSur-kde && sh ./install.sh && cd -
+    [[ "$?" != "0" ]] && return 1
+    cd $tmp_path/WhiteSur-kde/sddm && sudo sh ./install.sh && cd -
+
+    loginfo "安装Icons主题-[WhiteSur-icon-theme]"
+    git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git  $tmp_path/WhiteSur-icon-theme && cd $tmp_path/WhiteSur-icon-theme && sh ./install.sh && cd -
+    [[ "$?" != "0" ]] && return 1
+    loginfo "安装主题-[McMojave-circle]"
+    git clone https://github.com/vinceliuice/McMojave-circle.git  $tmp_path/McMojave-circle && cd $tmp_path/McMojave-circle && sh ./install.sh -all && cd -
+    [[ "$?" != "0" ]] && return 1
+    loginfo "安装GRUB2主题-[grub2-themes]"
+    git clone https://github.com/vinceliuice/grub2-themes.git  $tmp_path/grub2-themes && cd $tmp_path/grub2-themes && sudo ./install.sh -b -t whitesur && cd -
+
+    ! which lookandfeeltool >/dev/null && sudo $pac_cmd_ins plasma-workspace plasma5-workspace
+    ! which lookandfeeltool >/dev/null && logerr "lookandfeeltool 安装失败,请手动设置主题" && return 1
+    
+    # 主题设置
+    declare -a theme_namelist
+    idx=1
+    menu_head "当前系统安装的主题列表："
+    for str_item in `lookandfeeltool -l`
+    do
+        menu_iteml $idx $str_item
+        theme_namelist[$idx]="$str_item"
+        let idx=$idx+1
+    done
+    menu_tail
+    while true ; do
+        read -r -n 1 -e -p "请选择主题序号:" str_answer
+        [[ "$str_answer" != "" ]] && [[ $str_answer -gt 0 ]] && [[ $str_answer -lt "$idx" ]] && your_theme="${theme_namelist[$str_answer]}" && break
+    done
+    loginfo "您选择的主题为:$your_theme"
+    lookandfeeltool -a $your_theme
+    menu_tail
+    menu_iteml "提示" "更多设置请使用${BG}系统设置${NC}"
+    menu_tail
+    loginfo "成功执行 config_kde_theme"
+}
+function config_desktop_theme(){
+    loginfo "正在执行 config_desktop_theme 安装配置桌面主题"
+    prompt "开始安装桌面主题"
+    [[ "$?" != "0" ]] && return 1
+    menu_head "当前桌面环境检查"
+    menu_iteml "桌面环境" $gui_type
+    menu_iteml "操作系统" $os_type
+    menu_tail
+    case "$gui_type" in 
+        KDE)
+            config_kde_theme
+            ;;
+        *)
+            echo "$os_type 系统类型不支持！"
+            ;;
+    esac
+}
+
+function show_menu_desktop() { # 显示子菜单
+    menu_head "配置选项菜单"
+    menu_item 1 安装桌面主题
+    menu_tail
+    menu_item q 返回上级菜单
+    menu_tail
+}
+function do_desktop_all() {
+    while true
+    do
+        show_menu_desktop
+        read -r -n 1 -e  -p "`echo_greenr 请选择:` ${PMT} " str_answer
+        case "$str_answer" in
+            1) config_desktop_theme ;;  # 桌面主题配置
+
             q) return 0             ;;  # 返回上级菜单
             *) redr_line "没这个选择[$str_answer],搞错了再来." ;;
         esac
