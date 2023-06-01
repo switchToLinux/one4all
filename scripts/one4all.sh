42f376de216ae3fd99ba6d6ce51cd729adcebe12
Awkee <next4nextjob@gmail.com>
2023-06-01 15:55:51 +0800

    feat: 🎸 add kibina install

--- scripts/one4all.sh
+++ scripts/one4all.sh
@@ -968,11 +968,60 @@ function install_elasticsearch() {
     loginfo "运行命令:   \$ES_HOME/bin/elasticsearch -d -p /tmp/myes.pid"
     loginfo "停止命令:   pkill -F /tmp/myes.pid"
     loginfo "配置文件:   \$ES_HOME/config/elasticsearch.yml"
+    loginfo "配置 /etc/sysctl.conf"
+    # 配置 简单的配置
+    es_yml_file="$ES_HOME/config/elasticsearch.yml"
+    loginfo "配置 $es_yml_file"
+    cp -p $es_yml_file "${es_yml_file}.bak"
+    es_yml_data="bm9kZS5uYW1lOiAibm9kZTEwIgpwYXRoOgogIGRhdGE6IC9lczAxL2RhdGEKICBsb2dzOiAvZXMwMS9sb2cKICByZXBvOiAvYmFrMDEvcmVwbwoKbmV0d29yay5ob3N0OiAwLjAuMC4wCmh0dHAucG9ydDogOTIwMAoKYWN0aW9uLmRlc3RydWN0aXZlX3JlcXVpcmVzX25hbWU6IHRydWUKYm9vdHN0cmFwLm1lbW9yeV9sb2NrOiB0cnVlCmluZGljZXMuZmllbGRkYXRhLmNhY2hlLnNpemU6ICAxMCUKCiMtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLSBCRUdJTiBTRUNVUklUWSBBVVRPIENPTkZJR1VSQVRJT04gLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0KIwojIFRoZSBmb2xsb3dpbmcgc2V0dGluZ3MsIFRMUyBjZXJ0aWZpY2F0ZXMsIGFuZCBrZXlzIGhhdmUgYmVlbiBhdXRvbWF0aWNhbGx5ICAgICAgCiMgZ2VuZXJhdGVkIHRvIGNvbmZpZ3VyZSBFbGFzdGljc2VhcmNoIHNlY3VyaXR5IGZlYXR1cmVzIG9uIDE3LTA5LTIwMjIgMjM6MzM6MDgKIwojIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tCgojIEVuYWJsZSBzZWN1cml0eSBmZWF0dXJlcwp4cGFjay5zZWN1cml0eS5lbmFibGVkOiBmYWxzZQoKeHBhY2suc2VjdXJpdHkuZW5yb2xsbWVudC5lbmFibGVkOiBmYWxzZQoKIyBFbmFibGUgZW5jcnlwdGlvbiBmb3IgSFRUUCBBUEkgY2xpZW50IGNvbm5lY3Rpb25zLCBzdWNoIGFzIEtpYmFuYSwgTG9nc3Rhc2gsIGFuZCBBZ2VudHMKeHBhY2suc2VjdXJpdHkuaHR0cC5zc2w6CiAgZW5hYmxlZDogdHJ1ZQogIGtleXN0b3JlLnBhdGg6IGNlcnRzL2h0dHAucDEyCgojIEVuYWJsZSBlbmNyeXB0aW9uIGFuZCBtdXR1YWwgYXV0aGVudGljYXRpb24gYmV0d2VlbiBjbHVzdGVyIG5vZGVzCnhwYWNrLnNlY3VyaXR5LnRyYW5zcG9ydC5zc2w6CiAgZW5hYmxlZDogdHJ1ZQogIHZlcmlmaWNhdGlvbl9tb2RlOiBjZXJ0aWZpY2F0ZQogIGtleXN0b3JlLnBhdGg6IGNlcnRzL3RyYW5zcG9ydC5wMTIKICB0cnVzdHN0b3JlLnBhdGg6IGNlcnRzL3RyYW5zcG9ydC5wMTIKIyBDcmVhdGUgYSBuZXcgY2x1c3RlciB3aXRoIHRoZSBjdXJyZW50IG5vZGUgb25seQojIEFkZGl0aW9uYWwgbm9kZXMgY2FuIHN0aWxsIGpvaW4gdGhlIGNsdXN0ZXIgbGF0ZXIKY2x1c3Rlci5pbml0aWFsX21hc3Rlcl9ub2RlczogWyJub2RlMTAiXQoKIy0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIEVORCBTRUNVUklUWSBBVVRPIENPTkZJR1VSQVRJT04gLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLQoK"
+    echo -n "$es_yml_data" | base64 -d  > ${es_yml_file}
+    
+    loginfo "配置 /etc/sysctl.conf"
+    sysctl_data="CmZzLmlub3RpZnkubWF4X3VzZXJfaW5zdGFuY2VzPTIwNDgKZnMuaW5vdGlmeS5tYXhfdXNlcl93YXRjaGVzPTE1NTM1MAp2bS5tYXhfbWFwX2NvdW50PTQ2MjE0NAp2bS5taW5fZnJlZV9rYnl0ZXM9NTQ4NTc2CnZtLm92ZXJjb21taXRfbWVtb3J5ID0gMQo="
+    sudo sh -c "echo -n $sysctl_data | base64 -d >> /etc/sysctl.conf"
+    # 用户limit配置
+    uname=`whoami`
+    limit_file=/etc/security/limits.d/${uname}.conf
+    loginfo "配置 $limit_file"
+    sudo sh -c "echo $uname soft memlock unlimited >> ${limit_file}"
+    sudo sh -c "echo $uname hard memlock unlimited >> ${limit_file}"
+    loginfo "配置 Elasticsearch 完成!"
+}
+function install_kibana() {
+    es_url="https://artifacts.elastic.co/downloads/kibana/kibana-8.7.1-linux-x86_64.tar.gz"
+    es_sha512="https://artifacts.elastic.co/downloads/kibana/kibana-8.7.1-linux-x86_64.tar.gz.sha512"
+
+    install_path="${1:-.}"       # 默认安装路径
+    prompt "开始安装 Kibana...(默认安装位置为： ${install_path})"
+    if [ "$?" != "0" ] ; then
+        read -p "请输入安装目录:" install_path
+    fi
+    es_path="${install_path}/kibana-8.7.1/"
+    [[ -d "$install_path" ]] || mkdir $install_path || ( loginfo "目录创建失败!" && return 1 )
+    [[ -d "$es_path" ]] && loginfo "已经安装过了!" && return 1
+
+    tmp_dir="/tmp"
+    filename="`basename $es_url`"
+    curl -C - -o ${tmp_dir}/$filename ${es_url}
+    filesha512="`basename $es_sha512`"
+    curl -o ${tmp_dir}/${filesha512} ${es_sha512}
+    shasum -a 512 -c ${filesha512} || ( loginfo " sha512 签名验证失败" && return 1 )
+    
+    tar axvf ${tmp_dir}/${filename} -C $install_path  &&  loginfo "解压缩 $filename 文件到 $install_path 目录成功"
+    cd $es_path || ( echo "成功失败! 安装目录: $es_path" && return 1 )
+    loginfo "成功安装目录: $es_path"
+    loginfo "使用前请设置 环境变量(添加到 ~/.bashrc 文件): "
+    loginfo " export KIBANA_HOME=$es_path "
+    loginfo "运行命令:   \$KIBANA_HOME/bin/kibana"
+    loginfo "配置文件:   \$KIBANA_HOME/config/kibana.yml"
+    loginfo "配置 Elasticsearch 完成!"
 }
 function show_menu_develop() {
     menu_head "选项菜单"
     menu_item 1 安装SDWebUI
     menu_item 2 安装Elasticsearch
+    menu_item 3 安装Kibana
+    
     menu_tail
     menu_item q 返回上级菜单
     menu_tail
@@ -985,6 +1034,7 @@ function do_develop_all() {
         case "$str_answer" in
             1) install_sdwebui      ;;
             2) install_elasticsearch ;;
+            3) install_kibana  ;;
 
             q|"") return 0             ;;  # 返回上级菜单
             *) redr_line "没这个选择[$str_answer],搞错了再来." ;;
