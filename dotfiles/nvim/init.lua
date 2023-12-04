0132021f82c2ba993ab91b12ad3504f4b62d308a
zioer <next4nextjob@gmail.com>
2023-12-04 17:24:29 +0800

    feat: 支持安装配置neovim

---
+++ dotfiles/nvim/init.lua
@@ -0,0 +1,18 @@
+-- load options
+require('options')        
+
+
+-- load Packervim
+require('plugins')
+
+
+-- load keymappings
+require('keymaps')
+
+
+-- Set colorscheme
+require('colorscheme')
+
+
+-- Set LSP
+require('lsp')
