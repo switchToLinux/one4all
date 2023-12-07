902006a68219e89f033cfc0c47ad678c51ce8336
zioer <next4nextjob@gmail.com>
2023-12-07 20:07:51 +0800

    feat: 支持安装Logstash
    支持安装Logstash
    
    取消 sha512 验证(下载sha512文件 403拒绝访问)

--- scripts/all/main_install_dev.sh
+++ scripts/all/main_install_dev.sh
@@ -10,6 +10,10 @@
 #   - 
 ######################################################################
 
+# Elastic Stack Version
+
+ES_VERSION="8.7.1"
+
 ########## 开发环境 install_develop ##########################
 function install_sdwebui() {
     loginfo "开始执行 install_sdwebui, 安装源: https://github.com/AUTOMATIC1111/stable-diffusion-webui"
@@ -60,82 +64,92 @@ function install_sdwebui() {
 }
 
 function install_elasticsearch() {
-    es_url="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.7.1-linux-x86_64.tar.gz"
-    es_sha512="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.7.1-linux-x86_64.tar.gz.sha512"
-    install_path="${1:-.}"       # 默认安装路径
+
     prompt "开始安装 Elasticsearch..." || return 1
+
+    cur_username=`whoami`
+    prompt "是否确认在当前用户($cur_username)下安装Elasticsearch" || return 1
+    
+    es_url="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}-linux-x86_64.tar.gz"
+    install_path="${1:-.}"       # 默认安装路径
 
     read -p "请输入安装目录(默认安装位置：${install_path}):" str_path
     [[ "$str_path" != "" ]] || install_path=$str_path
     [[ ! -d "$install_path" ]] && ! mkdir $install_path && loginfo "目录创建失败!" && return 1
-    es_path="${install_path}/elasticsearch-8.7.1/"
+    es_path="${install_path}/elasticsearch-${ES_VERSION}/"
     [[ -d "$es_path" ]] && loginfo "已经安装过了!" && return 1
 
     tmp_dir="/tmp"
     filename="`basename $es_url`"
     curl -C - -o ${tmp_dir}/$filename ${es_url}
-    filesha512="`basename $es_sha512`"
-    curl -o ${tmp_dir}/${filesha512} ${es_sha512} && shasum -a 512 -c ${filesha512}
-    [[ "$?" != "0" ]] && loginfo " sha512 签名验证失败" && return 1
+    [[ "$?" != "0" ]] && loginfo "下载 ${filename} 失败!" && return 1
     
     tar axvf ${tmp_dir}/${filename} -C $install_path  &&  loginfo "解压缩 $filename 文件到 $install_path 目录成功"
     cd $es_path
     [[ "$?" != "0" ]] && echo "安装失败! 安装目录: $es_path" && return 1
     loginfo "成功安装目录: $es_path"
-    loginfo "使用前请设置 环境变量(添加到 ~/.bashrc 文件): "
-    loginfo " export ES_HOME=$es_path "
-    loginfo " export ES_JAVA_OPTS=\"-Xms512m -Xmx512m\""
-    loginfo "运行命令:   \$ES_HOME/bin/elasticsearch -d -p /tmp/myes.pid"
-    loginfo "停止命令:   pkill -F /tmp/myes.pid"
-    loginfo "配置文件:   \$ES_HOME/config/elasticsearch.yml"
-    loginfo "配置 /etc/sysctl.conf"
-    # 配置 简单的配置
-    es_yml_file="$ES_HOME/config/elasticsearch.yml"
-    loginfo "配置 $es_yml_file"
-    cp -p $es_yml_file "${es_yml_file}.bak"
-    es_yml_data="bm9kZS5uYW1lOiAibm9kZTEwIgpwYXRoOgogIGRhdGE6IC9lczAxL2RhdGEKICBsb2dzOiAvZXMwMS9sb2cKICByZXBvOiAvYmFrMDEvcmVwbwoKbmV0d29yay5ob3N0OiAwLjAuMC4wCmh0dHAucG9ydDogOTIwMAoKYWN0aW9uLmRlc3RydWN0aXZlX3JlcXVpcmVzX25hbWU6IHRydWUKYm9vdHN0cmFwLm1lbW9yeV9sb2NrOiB0cnVlCmluZGljZXMuZmllbGRkYXRhLmNhY2hlLnNpemU6ICAxMCUKCiMtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLSBCRUdJTiBTRUNVUklUWSBBVVRPIENPTkZJR1VSQVRJT04gLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0KIwojIFRoZSBmb2xsb3dpbmcgc2V0dGluZ3MsIFRMUyBjZXJ0aWZpY2F0ZXMsIGFuZCBrZXlzIGhhdmUgYmVlbiBhdXRvbWF0aWNhbGx5ICAgICAgCiMgZ2VuZXJhdGVkIHRvIGNvbmZpZ3VyZSBFbGFzdGljc2VhcmNoIHNlY3VyaXR5IGZlYXR1cmVzIG9uIDE3LTA5LTIwMjIgMjM6MzM6MDgKIwojIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tCgojIEVuYWJsZSBzZWN1cml0eSBmZWF0dXJlcwp4cGFjay5zZWN1cml0eS5lbmFibGVkOiBmYWxzZQoKeHBhY2suc2VjdXJpdHkuZW5yb2xsbWVudC5lbmFibGVkOiBmYWxzZQoKIyBFbmFibGUgZW5jcnlwdGlvbiBmb3IgSFRUUCBBUEkgY2xpZW50IGNvbm5lY3Rpb25zLCBzdWNoIGFzIEtpYmFuYSwgTG9nc3Rhc2gsIGFuZCBBZ2VudHMKeHBhY2suc2VjdXJpdHkuaHR0cC5zc2w6CiAgZW5hYmxlZDogdHJ1ZQogIGtleXN0b3JlLnBhdGg6IGNlcnRzL2h0dHAucDEyCgojIEVuYWJsZSBlbmNyeXB0aW9uIGFuZCBtdXR1YWwgYXV0aGVudGljYXRpb24gYmV0d2VlbiBjbHVzdGVyIG5vZGVzCnhwYWNrLnNlY3VyaXR5LnRyYW5zcG9ydC5zc2w6CiAgZW5hYmxlZDogdHJ1ZQogIHZlcmlmaWNhdGlvbl9tb2RlOiBjZXJ0aWZpY2F0ZQogIGtleXN0b3JlLnBhdGg6IGNlcnRzL3RyYW5zcG9ydC5wMTIKICB0cnVzdHN0b3JlLnBhdGg6IGNlcnRzL3RyYW5zcG9ydC5wMTIKIyBDcmVhdGUgYSBuZXcgY2x1c3RlciB3aXRoIHRoZSBjdXJyZW50IG5vZGUgb25seQojIEFkZGl0aW9uYWwgbm9kZXMgY2FuIHN0aWxsIGpvaW4gdGhlIGNsdXN0ZXIgbGF0ZXIKY2x1c3Rlci5pbml0aWFsX21hc3Rlcl9ub2RlczogWyJub2RlMTAiXQoKIy0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIEVORCBTRUNVUklUWSBBVVRPIENPTkZJR1VSQVRJT04gLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLQoK"
-    echo -n "$es_yml_data" | base64 -d  > ${es_yml_file}
-    
-    loginfo "配置 /etc/sysctl.conf"
-    sysctl_data="CmZzLmlub3RpZnkubWF4X3VzZXJfaW5zdGFuY2VzPTIwNDgKZnMuaW5vdGlmeS5tYXhfdXNlcl93YXRjaGVzPTE1NTM1MAp2bS5tYXhfbWFwX2NvdW50PTQ2MjE0NAp2bS5taW5fZnJlZV9rYnl0ZXM9NTQ4NTc2CnZtLm92ZXJjb21taXRfbWVtb3J5ID0gMQo="
-    sudo sh -c "echo -n $sysctl_data | base64 -d >> /etc/sysctl.conf"
-    # 用户limit配置
-    uname=`whoami`
-    limit_file=/etc/security/limits.d/${uname}.conf
-    loginfo "配置 $limit_file"
-    sudo sh -c "echo $uname soft memlock unlimited >> ${limit_file}"
-    sudo sh -c "echo $uname hard memlock unlimited >> ${limit_file}"
-    loginfo "配置 Elasticsearch 完成!"
+    echo "使用前请设置 环境变量(添加到 ~/.bashrc 文件)或者保存到自己编写的启动脚本中: "
+    echo "export ES_HOME=$es_path "
+    echo "export ES_JAVA_OPTS=\"-Xms512m -Xmx512m\""
+    echo "运行命令:   \$ES_HOME/bin/elasticsearch -d -p /tmp/myes.pid"
+    echo "停止命令:   pkill -F /tmp/myes.pid"
+    echo "配置文件:   \$ES_HOME/config/elasticsearch.yml"
+    echo "安装 Elasticsearch 完成!"
 }
 function install_kibana() {
-    es_url="https://artifacts.elastic.co/downloads/kibana/kibana-8.7.1-linux-x86_64.tar.gz"
-    es_sha512="https://artifacts.elastic.co/downloads/kibana/kibana-8.7.1-linux-x86_64.tar.gz.sha512"
+    es_url="https://artifacts.elastic.co/downloads/kibana/kibana-${ES_VERSION}-linux-x86_64.tar.gz"
 
     install_path="${1:-.}"       # 默认安装路径
     prompt "开始安装 Kibana...(默认安装位置为： ${install_path})"
     if [ "$?" != "0" ] ; then
         read -p "请输入安装目录:" install_path
     fi
-    es_path="${install_path}/kibana-8.7.1/"
+    es_path="${install_path}/kibana-${ES_VERSION}/"
     [[ ! -d "$install_path" ]] && ! mkdir $install_path && loginfo "目录创建失败!" && return 1
     [[ -d "$es_path" ]] && loginfo "已经安装过了!" && return 1
 
     tmp_dir="/tmp"
     filename="`basename $es_url`"
     curl -C - -o ${tmp_dir}/$filename ${es_url}
-    filesha512="`basename $es_sha512`"
-    curl -o ${tmp_dir}/${filesha512} ${es_sha512} && shasum -a 512 -c ${filesha512}
-    [[ "$?" != "0" ]] && loginfo " sha512 签名验证失败" && return 1
+    [[ "$?" != "0" ]] && loginfo "下载 ${filename} 失败" && return 1
     
     tar axvf ${tmp_dir}/${filename} -C $install_path  &&  loginfo "解压缩 $filename 文件到 $install_path 目录成功"
-    cd $es_path
+    cd $es_path && cd ../ && unlink kibana && ln -sf kibana-${ES_VERSION} kibana
     [[ "$?" != "0" ]] && echo "安装失败! 安装目录: $es_path" && return 1
     loginfo "成功安装目录: $es_path"
     loginfo "使用前请设置 环境变量(添加到 ~/.bashrc 文件): "
     loginfo " export KIBANA_HOME=$es_path "
     loginfo "运行命令:   \$KIBANA_HOME/bin/kibana"
     loginfo "配置文件:   \$KIBANA_HOME/config/kibana.yml"
-    loginfo "配置 Elasticsearch 完成!"
+    loginfo "安装 Kibana 完成!"
+}
+
+function install_logstash() {
+    es_url="https://artifacts.elastic.co/downloads/logstash/logstash-${ES_VERSION}-linux-x86_64.tar.gz"
+
+    install_path="${1:-.}"       # 默认安装路径
+    prompt "开始安装 logstash...(默认安装位置为： ${install_path})"
+    if [ "$?" != "0" ] ; then
+        read -p "请输入安装目录:" install_path
+    fi
+    es_path="${install_path}/logstash-${ES_VERSION}/"
+    [[ ! -d "$install_path" ]] && ! mkdir $install_path && loginfo "目录创建失败!" && return 1
+    [[ -d "$es_path" ]] && loginfo "已经安装过了!" && return 1
+
+    tmp_dir="/tmp"
+    filename="`basename $es_url`"
+    curl -C - -o ${tmp_dir}/$filename ${es_url}
+    [[ "$?" != "0" ]] && loginfo "下载 ${filename} 失败" && return 1
+    
+    tar axvf ${tmp_dir}/${filename} -C $install_path  &&  loginfo "解压缩 $filename 文件到 $install_path 目录成功"
+    cd $es_path && cd ../ && unlink logstash && ln -sf logstash-${ES_VERSION} logstash
+    [[ "$?" != "0" ]] && echo "安装失败! 安装目录: $es_path" && return 1
+    loginfo "成功安装目录: $es_path"
+    loginfo "使用前请设置 环境变量(添加到 ~/.bashrc 文件): "
+    loginfo " export LOGSTASH_HOME=$es_path "
+    loginfo "运行命令:   \$LOGSTASH_HOME/bin/logstash -f xxx.conf"
+    loginfo "配置文件:   \$LOGSTASH_HOME/config"
+    loginfo "安装 Logstash 完成!"
 }
 function install_nodejs() {
     nodejs_type="${1:-LTS}"   # nodejs 类型 LTS 或 latest最新版
@@ -216,6 +230,7 @@ function show_menu_develop() {
     menu_item 3 GoLang
     menu_item 4 安装Elasticsearch
     menu_item 5 安装Kibana
+    menu_item 6 安装Logstash
     menu_tail
     menu_item q 返回上级菜单
     menu_tail
@@ -231,6 +246,7 @@ function do_develop_all() {
             3) install_golang       ;;
             4) install_elasticsearch ;;
             5) install_kibana  ;;
+            6) install_logstash ;;
 
             q|"") return 0             ;;  # 返回上级菜单
             *) redr_line "没这个选择[$str_answer],搞错了再来." ;;
